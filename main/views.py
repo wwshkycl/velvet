c471fd6d9a3acb4272c4bcd1968ff9450f8bdcab
140zvezd <aminism09@gmail.com>
2025-12-23 14:01:11 +0300

    init project and create models

---
+++ main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from .models import Category, Size, Product
+from django.db.models import Q
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['sizes'] = Size.objects.all()
+        context['current_category'] = None
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/home_content.html', context)
+        return TemplateResponse(request, self.template_name, context)
+
+
+class CatalogView(TemplateView):
+    template_name = 'main/base.html'
+
+    FILTER_MAPPING = {
+
+    }
